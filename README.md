4d7f6ce96b65d0845456c6725856e696cd494303
KendrickPC <kennethpchang@gmail.com>
2017-06-16 12:33:13 +0800

    NBA Finals highlights edited

--- README.md
+++ README.md
@@ -1,10 +1,41 @@
-README.md
+<!-- Instructions: -->
 
+  1. Open entertainmentCenter.py in IDLE (python).
+  2. Under the run tab, click "run module"
+
+<!-- Enjoy NBA Highlights! -->
+
+Notes for myself:
+<!-- README.md -->
 * Google style guide prefers class names to begin with a capital letter.
 http://google.github.io/styleguide/pyguide.html
 
-make an instance of class Movie(): 
+make an instance of class Movie():
 
-Keeping class definitions in one file (media.py) and call definitions in (entertainmentCenter.py). 
+Keeping class definitions in one file (media.py) and call definitions in
+the entertainmentCenter.py file.
 
 A class can have both data and methods
+
+Attempt to integrate python template code to yeoman webapp build.
+  add the bootstrap-theme.min.css file
+    https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css
+
+Discovery: Front end dependencies are not necessary for this webapp.
+
+*** Run entertainmentCenter.py in IDLE to update the page ***
+
+<!-- Task #1: -->
+	- Remove the CDN (content developer network) from the code block in the
+  fresh_tomatoes.py
+
+  - create js and styles files
+
+  - Change media links for the youtube embed and update the
+  Wiki images with the player of the game.
+
+Movie images are the following size:
+width: 220px;
+height: 342px;
+
+Load the desired images onto the server and pull from the server instead of a CDN/embed method
--- entertainmentCenter.py
+++ entertainmentCenter.py
@@ -1,44 +1,121 @@
 import fresh_tomatoes
 import media
 
-toy_story = media.Movie("Toy Story",
-						"A story about a boy and his toys that come to life", 
-						"https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", 
-						"https://www.youtube.com/watch?v=KYz2wyBy3kc"
+# begin 2015 season
+NBA_Finals_2015_game_1 = media.Movie("2015 NBA Finals game 1 ",
+						"A story about a boy and his toys that come to life",
+						"https://cdn-s3.si.com/s3fs-public/2015/06/05/lebron-james-cavaliers-warriors-2015-nba-finals-game-1.jpg",
+						""
 	)
-# print(toy_story.storyline)
 
-spirited_away = media.Movie("Spirited Away",
-						"During her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches, and spirits, and where humans are changed into beasts.", 
-						"http://vignette2.wikia.nocookie.net/studio-ghibli/images/6/64/Spirited_Away_%28Svensk_DVD%29.jpg/revision/latest?cb=20140116135517",
-						"https://www.youtube.com/watch?v=ByXuk9QqQkk"
+NBA_Finals_2015_game_2 = media.Movie("2015 NBA Finals game 2",
+						"During her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches, and spirits, and where humans are changed into beasts.",
+						"https://static.sportskeeda.com/wp-content/uploads/2015/06/lebron9-1433741652-800.jpg",
+						""
 	)
-# spirited_away.show_trailer()
 
-howls_moving_castle = media.Movie("Howl's Moving Castle",
+NBA_Finals_2015_game_3 = media.Movie("2015 NBA Finals game 3",
 						"When an unconfident young woman is cursed with an old body by a spiteful witch, her only chance of breaking the spell lies with a self-indulgent yet insecure young wizard and his companions in his legged, walking castle.",
-						"https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-12561-1uvi0ti_281fe89f.jpeg?region=0%2C0%2C1000%2C1363",
-						"https://www.youtube.com/watch?v=iwROgK94zcM"
+						"https://cdn-s3.si.com/s3fs-public/images/NBA-Finals-Game-3-u.jpg",
+						""
 	)
-# howls_moving_castle.show_trailer()
 
-princess_mononoke = media.Movie("Princess Mononoke",
+NBA_Finals_2015_game_4 = media.Movie("2015 NBA Finals game 4",
 						"On a journey to find the cure for a Tatarigami's curse, Ashitaka finds himself in the middle of a war between the forest gods and Tatara, a mining colony. In this quest he also meets San, the Mononoke Hime.",
-						"https://vignette4.wikia.nocookie.net/toonami/images/c/c6/Princess_Mononoke.jpg/revision/latest?cb=20130829185423",
-						"https://www.youtube.com/watch?v=4OiMOHRDs14"
+						"http://img.bleacherreport.net/img/images/photos/003/432/347/hi-res-82252136f1c5cc4d9616cf1dfac58210_crop_north.jpg?h=533&w=800&q=70&crop_x=center&crop_y=top",
+						""
 	)
 
-my_neighbor_totoro = media.Movie("My Neighbor Totoro",
+NBA_Finals_2015_game_5 = media.Movie("2015 NBA Finals game 5",
 						"When two girls move to the country to be near their ailing mother, they have adventures with the wondrous forest spirits who live nearby.",
-						"https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-12561-1uvi0ti_281fe89f.jpeg?region=0%2C0%2C1000%2C1363",
-						"https://www.youtube.com/watch?v=B6B_nuHksvs"
+						"https://www.sikids.com/sites/default/files/sikids/pages/images/cms/imce/users/dantec/2015/06/nba-finals-2015-game5-header.jpg",
+						""
 	)
 
-grave_of_the_fireflies = media.Movie("Grave of the Fireflies",
+NBA_Finals_2015_game_6 = media.Movie("2015 NBA Finals game 6",
 						"A young boy and his little sister struggle to survive in Japan during World War II.",
-						"http://static.rogerebert.com/uploads/movie/movie_poster/grave-of-the-fireflies-1988/large_bwVhmPpydv8P7mWfrmL3XVw0MV5.jpg",
-						"https://www.youtube.com/watch?v=4vPeTSRd580"
+						"http://img.bleacherreport.net/img/images/photos/003/439/993/67d124e856d72ff6bef91327bf32636f_crop_north.jpg?1434515317&w=630&h=420",
+						""
+	)
+# end 2015 season
+# begin 2016 season
+NBA_Finals_2016_game_1 = media.Movie("2016 NBA Finals game 1",
+						"A story about a boy and his toys that come to life",
+						"https://pmcdeadline2.files.wordpress.com/2016/06/nba-finals-2016-game-1-june-2-2016.jpg?w=446&h=299&crop=1",
+						""
 	)
 
-movies = [toy_story, spirited_away, howls_moving_castle, princess_mononoke, my_neighbor_totoro, grave_of_the_fireflies]
+NBA_Finals_2016_game_2 = media.Movie("2016 NBA Finals game 2",
+						"During her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches, and spirits, and where humans are changed into beasts.",
+						"http://i.cdn.turner.com/drp/nba/warriors/sites/default/files/getty-images-538304998.jpg",
+						""
+	)
+
+NBA_Finals_2016_game_3 = media.Movie("2016 NBA Finals game 3",
+						"When an unconfident young woman is cursed with an old body by a spiteful witch, her only chance of breaking the spell lies with a self-indulgent yet insecure young wizard and his companions in his legged, walking castle.",
+						"http://picture.youth.cn/qtdb/201606/W020160617430024529933.jpg",
+						""
+	)
+
+NBA_Finals_2016_game_4 = media.Movie("2016 NBA Finals game 4",
+						"On a journey to find the cure for a Tatarigami's curse, Ashitaka finds himself in the middle of a war between the forest gods and Tatara, a mining colony. In this quest he also meets San, the Mononoke Hime.",
+						"http://media.cleveland.com/plain-dealer/photo/2016/06/11/-b7ec6760e398c3d5.jpg",
+						""
+	)
+
+NBA_Finals_2016_game_5 = media.Movie("2016 NBA Finals game 5",
+						"When two girls move to the country to be near their ailing mother, they have adventures with the wondrous forest spirits who live nearby.",
+						"https://cdn.nba.net/nba-drupal-prod/styles/tile_640w/http/i2.cdn.turner.com/nba/nba/dam/assets/160614103926-lebron-james-kyrie-irving-2016-nba-finals---game-five.1280x720.jpeg?itok=78_1FdtO",
+						""
+	)
+
+NBA_Finals_2016_game_6 = media.Movie("2016 NBA Finals game 6",
+						"A young boy and his little sister struggle to survive in Japan during World War II.",
+						"http://img15.hostingpics.net/pics/924545133857848867104114521691518969397n.jpg",
+						""
+	)
+
+NBA_Finals_2016_game_7 = media.Movie("2016 NBA Finals game 7",
+						"A young boy and his little sister struggle to survive in Japan during World War II.",
+						"https://i.ytimg.com/vi/jPcpJ_J574A/maxresdefault.jpg",
+						""
+	)
+# end 2016 season
+# begin 2017 season
+NBA_Finals_2017_game_1 = media.Movie("2017 NBA Finals game 1",
+						"A story about a boy and his toys that come to life",
+						"http://media.gettyimages.com/photos/kevin-durant-of-the-golden-state-warriors-reacts-to-a-play-against-picture-id691331486",
+						""
+	)
+
+NBA_Finals_2017_game_2 = media.Movie("2017 NBA Finals game 2",
+						"During her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches, and spirits, and where humans are changed into beasts.",
+						"https://cdn.nba.net/nba-drupal-prod/styles/tile_640w/http/nba.cdn.turner.com/nba/big/video/2017/06/05/dcc99633-50de-4e03-a1ee-b95eb487b03d.nba_1_1280x720.jpg?itok=FiFLnt8i",
+						""
+	)
+
+NBA_Finals_2017_game_3 = media.Movie("2017 NBA Finals game 3",
+						"When an unconfident young woman is cursed with an old body by a spiteful witch, her only chance of breaking the spell lies with a self-indulgent yet insecure young wizard and his companions in his legged, walking castle.",
+						"https://img.bleacherreport.net/img/images/photos/003/678/388/hi-res-102088badb96e968ad486d23e51d1a7c_crop_north.jpg?h=533&w=800&q=70&crop_x=center&crop_y=top",
+						""
+	)
+
+NBA_Finals_2017_game_4 = media.Movie("2017 NBA Finals game 4",
+						"On a journey to find the cure for a Tatarigami's curse, Ashitaka finds himself in the middle of a war between the forest gods and Tatara, a mining colony. In this quest he also meets San, the Mononoke Hime.",
+						"http://images.complex.com/complex/images/fl_lossy,q_auto/v1/xabzj1kuigykvittlztf/kd-lebron-draymond-zaza-game-4-nba-finals",
+						""
+	)
+
+NBA_Finals_2017_game_5 = media.Movie("2017 NBA Finals game 5",
+						"When two girls move to the country to be near their ailing mother, they have adventures with the wondrous forest spirits who live nearby.",
+						"https://cdn.nba.net/nba-drupal-prod/styles/landscape/s3/2017-06/GettyImages-695396452%20%281%29.jpg?itok=7jgqcrcP",
+						""
+	)
+# end 2017 season
+
+movies = [NBA_Finals_2015_game_1, NBA_Finals_2015_game_2, NBA_Finals_2015_game_3, NBA_Finals_2015_game_4, NBA_Finals_2015_game_5, NBA_Finals_2015_game_6,
+NBA_Finals_2016_game_1, NBA_Finals_2016_game_2, NBA_Finals_2016_game_3, NBA_Finals_2016_game_4, NBA_Finals_2016_game_5, NBA_Finals_2016_game_6, NBA_Finals_2016_game_7,
+NBA_Finals_2017_game_1, NBA_Finals_2017_game_2, NBA_Finals_2017_game_3, NBA_Finals_2017_game_4, NBA_Finals_2017_game_5
+]
+
 fresh_tomatoes.open_movies_page(movies)
